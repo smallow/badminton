90b9c4a3e431c9a2d14316dba32fc2edefbb31b5
smallow <playdreamist@gmail.com>
2015-11-09 01:04:18 +0800

    77

--- app/src/main/java/com/smallow/badminton/SplashActivity.java
+++ app/src/main/java/com/smallow/badminton/SplashActivity.java
@@ -1,55 +1,129 @@
 package com.smallow.badminton;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.BinaryHttpResponseHandler;
+import com.smallow.badminton.sys.Constant;
 import com.smallow.badminton.sys.base.BaseActivity;
-import com.smallow.badminton.sys.ui.CustomProgressDialogOne;
-import com.smallow.badminton.sys.ui.CustomProgressDialogThree;
-import com.smallow.badminton.sys.ui.CustomProgressDialogTwo;
+import com.smallow.badminton.sys.utils.HttpUtils;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2015/10/28.
  */
 public class SplashActivity extends BaseActivity {
 
-    private Button button;
-    private CustomProgressDialogOne progressDialogOne;
-    private CustomProgressDialogTwo progressDialogTwo;
-    private CustomProgressDialogThree progressDialogThree;
+    private ImageView iv_start;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_splash);
-        progressDialogOne = new CustomProgressDialogOne(this, "客官请稍后...");
-        progressDialogTwo = new CustomProgressDialogTwo(this, "客官请稍后...");
-        progressDialogThree = new CustomProgressDialogThree(this);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        iv_start = (ImageView) findViewById(R.id.iv_start);
+        initImage();
+    }
+
+    private void initImage() {
+        File dir = getFilesDir();
+        final File imgFile = new File(dir, "start.jpg");
+        if (imgFile.exists()) {
+            iv_start.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
+        } else {
+            iv_start.setImageDrawable(getResources().getDrawable(R.drawable.start));
+        }
+
+
+        final ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        scaleAnim.setFillAfter(true);
+        scaleAnim.setDuration(3000);
+        scaleAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View v) {
-                /*if(progressDialogOne!=null && !progressDialogOne.isShowing()){
-                    progressDialogOne.show();
-                }else{
-                    progressDialogOne.dismiss();
-                }*/
+            public void onAnimationStart(Animation animation) {
 
-                /*if(progressDialogTwo!=null && !progressDialogTwo.isShowing()){
-                    progressDialogTwo.show();
-                }else{
-                    progressDialogTwo.dismiss();
-                }*/
+            }
 
-                if (progressDialogThree != null && !progressDialogThree.isShowing()) {
-                    progressDialogThree.setMsg("客官别急请稍后...");
-                    progressDialogThree.show();
-                } else {
-                    progressDialogThree.dismiss();
-                }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                HttpUtils.get(Constant.START, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(new String(responseBody));
+                            String url = jsonObject.getString("img");
+                            HttpUtils.get(url, new BinaryHttpResponseHandler() {
+                                @Override
+                                public void onSuccess(int statusCode, Header[] headers, byte[] binaryData) {
+                                    saveImage(imgFile, binaryData);
+                                    startActivity();
+                                }
+
+                                @Override
+                                public void onFailure(int statusCode, Header[] headers, byte[] binaryData, Throwable error) {
+                                    startActivity();
+                                }
+                            });
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        startActivity();
+                    }
+                });
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
 
             }
         });
+        iv_start.startAnimation(scaleAnim);
+
+    }
+
+
+
+    private void startActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in,
+                android.R.anim.fade_out);
+        finish();
+    }
+
+    public void saveImage(File file, byte[] bytes) {
+        try {
+            file.delete();
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
