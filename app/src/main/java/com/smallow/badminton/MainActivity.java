862d0401ddf8abf3d5e4fdf777cc80f955d3119c
smallow <playdreamist@gmail.com>
2015-11-09 01:07:00 +0800

    2015-11-08

--- app/src/main/java/com/smallow/badminton/MainActivity.java
+++ app/src/main/java/com/smallow/badminton/MainActivity.java
@@ -1,38 +1,60 @@
 package com.smallow.badminton;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+import com.smallow.badminton.sys.base.BaseActivity;
 
+
+public class MainActivity extends AppCompatActivity {
+    //#F14E41
+    private long firstTime;
+    private Toolbar toolbar;
+    private boolean isLight;
+    private SharedPreferences sp;
+    private FrameLayout fl_content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        isLight = sp.getBoolean("isLight", true);
+        initView();
     }
 
+    private void initView() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("爱羽玩");
+        setSupportActionBar(toolbar);
+        fl_content = (FrameLayout) findViewById(R.id.fl_content);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.getItem(0).setTitle(sp.getBoolean("isLight", true) ? "夜间模式" : "日间模式");
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    public void onBackPressed() {
+        long secondTime = System.currentTimeMillis();
+        if (secondTime - firstTime > 2000) {
+            Snackbar sb = Snackbar.make(fl_content, "再按一次退出", Snackbar.LENGTH_SHORT);
+            sb.getView().setBackgroundColor(getResources().getColor(isLight ? android.R.color.holo_red_dark : android.R.color.black));
+            sb.show();
+            firstTime = secondTime;
+        } else {
+            finish();
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/smallow/badminton/sys/Constant.java
@@ -0,0 +1,10 @@
+package com.smallow.badminton.sys;
+
+/**
+ * Created by smallow on 15/11/8.
+ */
+public class Constant {
+
+    public static final String BASEURL = "http://news-at.zhihu.com/api/4/";
+    public static final String START = "start-image/1080*1776";
+}
