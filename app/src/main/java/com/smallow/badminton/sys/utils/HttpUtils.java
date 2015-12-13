0e7867bffa607287ee2de9f36e789a366267d498
smallow <playdreamist@gmail.com>
2015-12-14 01:34:32 +0800

    2015-12-13

--- app/src/main/java/com/smallow/badminton/sys/utils/HttpUtils.java
+++ app/src/main/java/com/smallow/badminton/sys/utils/HttpUtils.java
@@ -5,6 +5,10 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.BinaryHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.ResponseHandlerInterface;
 import com.smallow.badminton.sys.Constant;
 
@@ -14,12 +18,33 @@ import com.smallow.badminton.sys.Constant;
 public class HttpUtils {
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-
-    public static void get(String url, ResponseHandlerInterface responseHandler) {
-        client.get(Constant.BASEURL + url, responseHandler);
+    static {
+        client.setTimeout(20000);   //设置链接超时，如果不设置，默认为10s
     }
-    public static void getImage(String url, ResponseHandlerInterface responseHandler) {
-        client.get(url, responseHandler);
+
+    public static void get(String urlString, AsyncHttpResponseHandler res)    //用一个完整url获取一个string对象
+    {
+        client.get(urlString, res);
+    }
+
+    public static void get(String urlString, RequestParams params, AsyncHttpResponseHandler res)   //url里面带参数
+    {
+        client.get(urlString, params, res);
+    }
+
+    public static void get(String urlString, JsonHttpResponseHandler res)   //不带参数，获取json对象或者数组
+    {
+        client.get(urlString, res);
+    }
+
+    public static void get(String urlString, RequestParams params, JsonHttpResponseHandler res)   //带参数，获取json对象或者数组
+    {
+        client.get(urlString, params, res);
+    }
+
+    public static void get(String uString, BinaryHttpResponseHandler bHandler)   //下载数据使用，会返回byte数据
+    {
+        client.get(uString, bHandler);
     }
 
     public static boolean isNetworkConnected(Context context) {
